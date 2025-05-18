7754d7050639a88dde1be2d4b286154fd10d7b67
Kabir Sidhu <kxbir414@gmail.com>
2025-05-18 14:18:52 -0700

    init commit

---
+++ src/main/java/com/altiora/altiora/AltioraApplication.java
@@ -0,0 +1,13 @@
+package com.altiora.altiora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltioraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltioraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altiora/altiora/AltioraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altiora.altiora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltioraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
